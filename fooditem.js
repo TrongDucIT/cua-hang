45acdb4682c9531538403d6a9de5c71711b46113
Trong Duc <trongduck60.dee.utc@gmail.com>
2024-05-22 16:38:08 +0700

    second

---
+++ fooditem.js
@@ -0,0 +1,256 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Mực lá',
+    category : 'mực',
+    rating : 4.3,
+    price: 0,
+    img: 'images/muc/muc la.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Mực ống',
+    category : 'mực',
+    rating : 4.3,
+    price: 0,
+    img: 'images/muc/muc ong.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Mực trứng',
+    category : 'mực',
+    rating : 4.3,
+    price: 0,
+    img: 'images/muc/muc trung.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Mực sim',
+    category : 'mực',
+    rating : 4.3,
+    price: 0,
+    img: 'images/muc/muc sim.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mực mai',
+    category : 'mực',
+    rating : 4.3,
+    price: 0,
+    img: 'images/muc/muc mai.jpg',
+    quantity: 1
+},
+
+{
+    id: 10,
+    name: 'Tôm Sú',
+    category : 'tôm',
+    rating : 4.3,
+    price: 0,
+    img: 'images/tom/tom-su.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Tôm đất',
+    category : 'tôm',
+    rating : 4.3,
+    price: 0,
+    img: 'images/tom/tom dat.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Tôm sắt',
+    category : 'tôm',
+    rating : 4.3,
+    price: 0,
+    img: 'images/tom/tom sat.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Tôm hùm',
+    category : 'tôm',
+    rating : 4.3,
+    price: 0,
+    img: 'images/tom/tmo hum.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Tôm càng xanh',
+    category : 'tôm',
+    rating : 4.3,
+    price: 0,
+    img: 'images/tom/tom cang xanh.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Bề Bề',
+    category : 'tôm',
+    rating : 4.3,
+    price: 0,
+    img: 'images/tom/be be.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Cá chỉ vàng',
+    category : 'cá',
+    rating : 4.3,
+    price: 0,
+    img: 'images/ca/ca chi vang.jpeg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Cá nục',
+    category : 'cá',
+    rating : 4.3,
+    price: 0,
+    img: 'images/ca/ca nuc.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Cá thu',
+    category : 'cá',
+    rating : 4.3,
+    price: 0,
+    img: 'images/ca/ca thu.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Cá hồi',
+    category : 'cá',
+    rating : 4.3,
+    price: 0,
+    img: 'images/ca/dau ca hoi.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Cá mối',
+    category : 'cá',
+    rating : 0,
+    price: 8,
+    img: 'images/ca/ca moi.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Cá hồng',
+    category : 'cá',
+    rating : 4.3,
+    price: 0,
+    img: 'images/ca/ca hong.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Cá khoai',
+    age: '32',
+    category : 'cá',
+    rating : 4.3,
+    price: 0,
+    img: 'images/ca/ca khoai.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Chả mực',
+    category : 'Chả',
+    rating : 4.3,
+    price: 0,
+    img: 'images/cha/cha muc.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Chả cá',
+    category : 'Chả',
+    rating : 4.3,
+    price: 0,
+    img: 'images/cha/cha ca.jpg',
+    quantity: 1
+},
+
+{
+    id: 30,
+    name: 'Nước mắm Cát Hải',
+    category : 'mắm',
+    rating : 4.3,
+    price: 0,
+    img: 'images/mam/mam cat hai.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Mắm tôm',
+    category : 'mắm',
+    rating : 4.3,
+    price: 0,
+    img: 'images/mam/mam tom.jpg',
+    quantity: 1
+    },
+
+
+{
+    id: 36,
+    name: 'Nước mắm sơn hải',
+    category : 'mắm',
+    rating : 4.3,
+    price: 0,
+    img: 'images/mam/son hai.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Tôm khô',
+    category : 'đồ-khô',
+    rating : 4.3,
+    price: 0,
+    img: 'images/kho/tom kho.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Bò khô',
+    category : 'đồ-khô',
+    rating : 4.3,
+    price: 0,
+    img: 'images/kho/bo kho.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Gà khô',
+    category : 'đồ-khô',
+    rating : 4.3,
+    price: 0,
+    img: 'images/kho/ga kho.jpg',
+    quantity: 1
+},
+
+{
+    id: 44,
+    name: 'Ruốc tôm',
+    category : 'đồ-khô',
+    rating : 4.3,
+    price: 0,
+    img: 'images/kho/ruoc-tom.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
